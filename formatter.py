4595579ea1dd2a42f60f141a03f1ea18242c8345
Edinunzio <e.dinunzio@gmail.com>
2015-02-25 17:52:52 -0500

    Testing for empty files, returning proper list format

--- formatter.py
+++ formatter.py
@@ -5,8 +5,29 @@ class Formatter(object):
         f.close()
         return txt
 
+    def get_entries_by_line(self, file_contents):
+        """grabs content of each line and appended to array"""
+        entries = file_contents.split("\n")
+        container = []
+        for e in entries:
+            x = e.split(",")
+            container.append(x)
+        return container
+
+    '''def analyze_entry(self, entries):
+        """analyzes each entry"""
+        container = []
+        for e in entries:
+            x = e.split(",")
+            container.append(x)
+        return container'''
 
 if __name__ == '__main__':
     _fm = Formatter()
-    result = _fm.read_file('data/sample-Liz.in')
+    #result = _fm.read_file('data/empty.txt')
+    r_f = _fm.read_file('data/sample-Liz.in')
+    entries = _fm.get_entries_by_line(r_f)
+    result = entries
+    #result = _fm.analyze_entry(entries)
+    #result = _fm.get_entries_by_line(r_f)
     print result
--- test/test_formatter.py
+++ test/test_formatter.py
@@ -5,9 +5,16 @@ from formatter import Formatter
 class TestFormatter(unittest.TestCase):
     def setUp(self):
         self.fm = Formatter()
+        self.contents = self.fm.read_file('../data/sample-Liz.in')
 
     def test_read_file_method_returns_correct_type(self):
-        self.assertEqual(str, type(self.fm.read_file('../data/sample-Liz.in')))
+        #self.assertEqual(str, type(self.fm.read_file('../data/empty.txt')))
+        self.assertEqual(str, type(self.contents))
 
+    def test_read_file_method_returns_not_empty(self):
+        self.assertGreater(len(self.contents), 0)
+
+    def test_get_entries_by_line_method_returns_correct_type(self):
+        self.assertEqual(list, type(self.fm.get_entries_by_line(self.contents)))
 if __name__ == '__main__':
     unittest.main()
