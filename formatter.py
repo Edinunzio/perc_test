c1229212de8a45f62f89a95c4b2082a02c9d6dda
Edinunzio <e.dinunzio@gmail.com>
2015-02-25 17:09:51 -0500

    Defined read_file method and unittest

--- formatter.py
+++ formatter.py
@@ -1,1 +1,12 @@
+class Formatter(object):
+    def read_file(self, file):
+        f = open(file, 'r')
+        txt = f.read()
+        f.close()
+        return txt
 
+
+if __name__ == '__main__':
+    _fm = Formatter()
+    result = _fm.read_file('data/sample-Liz.in')
+    print result
--- test/test_formatter.py
+++ test/test_formatter.py
@@ -1,1 +1,13 @@
 import unittest
+from formatter import Formatter
+
+
+class TestFormatter(unittest.TestCase):
+    def setUp(self):
+        self.fm = Formatter()
+
+    def test_read_file_method_returns_correct_type(self):
+        self.assertEqual(str, type(self.fm.read_file('../data/sample-Liz.in')))
+
+if __name__ == '__main__':
+    unittest.main()
